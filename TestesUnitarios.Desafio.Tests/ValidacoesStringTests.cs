605775f3a4f7b23075bc4fcc941c9d536fea1597
osidsouza <s.p.souza@hotmail.com>
2024-01-09 00:24:39 -0300

    V1

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -1,4 +1,5 @@
 using TestesUnitarios.Desafio.Console.Services;
+using Xunit;
 
 namespace TestesUnitarios.Desafio.Tests;
 
@@ -7,13 +8,13 @@ public class ValidacoesStringTests
     private ValidacoesString _validacoes = new ValidacoesString();
 
     [Fact]
-    public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+    public void DeveRetornarQuantidadeCaracteresDaPalavraMatrix()
     {
-        //TODO: Corrigir a variável "texto" e "resultadoEsperado" da seção Arrange
+        // * IMPLEMENTADO *
 
         // Arrange
-        var texto = "a";
-        var resultadoEsperado = 0;
+        var texto = "matrix";
+        var resultadoEsperado = 6;
 
         // Act
         var resultado = _validacoes.RetornarQuantidadeCaracteres(texto);
@@ -29,13 +30,13 @@ public class ValidacoesStringTests
         var texto = "Esse é um texto qualquer";
         var textoProcurado = "qualquer";
 
-        //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
+        // * IMPLEMENTADO *
         // Act
-         _validacoes.ContemCaractere(texto, textoProcurado);
+        var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
-        //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
-        Assert.True(true);
+        // * IMPLEMENTADO *
+        Assert.True(resultado);
     }
 
     [Fact]
@@ -49,18 +50,19 @@ public class ValidacoesStringTests
         var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
         // Assert
-        //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
+        // * IMPLEMENTADO *
+        Assert.False(resultado);
     }
 
-    //TODO: Corrigir a anotação [Fact]
+    // * IMPLEMENTADO *
+    [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
     {
-        //TODO: Corrigir a variável "textoProcurado" seção Arrange
+        // * IMPLEMENTADO *
 
         // Arrange
         var texto = "Começo, meio e fim do texto procurado";
-        var textoProcurado = "teste";
+        var textoProcurado = "procurado";
 
         // Act
         var resultado = _validacoes.TextoTerminaCom(texto, textoProcurado);
